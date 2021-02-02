18f1662a14462d35851952fa128c5b7427bf8528
Jun Wakamatsu <jun.waka.fukuoka@gmail.com>
2021-02-02 14:51:21 +0000

    POSTハンドラ（登録機能）を追加

--- app.js
+++ app.js
@@ -17,6 +17,8 @@ let todos = [
 
 const app = express()
 
+app.use(express.json())
+
 // ToDo一覧の取得
 app.get('/api/todos', (req, res) => {
   if (!req.query.completed) {
@@ -25,6 +27,31 @@ app.get('/api/todos', (req, res) => {
   // completedクエリパラメータを指定された場合はToDoをフィルタリング
   const completed = req.query.completed === 'true'
   res.json(todos.filter(todo => todo.completed === completed))
+})
+
+// ToDoのIDを管理する変数（仮）
+let id = 2;
+
+// ToDoの作成
+app.post('/api/todos', (req, res, next) => {
+  const { title } = req.body
+  if (typeof title !== 'string' || !title) {
+    // titleがリクエストに含まれない場合はステータスコード400(Bad Request)
+    const err = new Error('title is required')
+    err.statusCode = 400
+    return next(err)
+  }
+  // ToDoの作成
+  const todo = { id: id += 1, title, completed: false }
+  todos.push(todo)
+  // ステータスコード201(Created)で結果を返す
+  res.status(201).json(todo)
+})
+
+// エラーハンドリングミドルウェア
+app.use((err, req, res, next) => {
+  console.error(err)
+  res.status(err.statusCode || 500).json({ error: err.message })
 })
 
 app.listen(3000)
